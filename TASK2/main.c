bfca750e2c507c0962709d90516bff345a41af72
Oleg200447 <113237437+Oleg200447@users.noreply.github.com>
2022-11-30 23:45:38 +0300

    Add files via upload

---
+++ TASK2/main.c
@@ -0,0 +1,30 @@
+﻿#include"sort.h"
+#include<stdio.h>
+
+int main()
+{
+	int** mas, n_row = 0, n_col = 0, choise = -1;
+	printf("If you want random input 1. If you want input masive by hands input 0.\n");
+	choiseFunction(&choise);
+	printf("Input how much rows in masive[1-100]:");
+	inputChekerRow(&n_row);
+	printf("Input how much columns in masive[1-100]:");
+	inputChekerColumn(&n_col);
+	mas = memory(n_row, n_col);
+	if (choise == 0)
+	{
+		printf("All elements should be in range[-1000;1000].\n");
+		inputMasByHand(mas, n_row, n_col);
+	}
+	if (choise == 1)
+	{
+		inputMasRandom(mas, n_row, n_col);
+	}
+	printf("Masive:");
+	showMas(mas, n_row, n_col);
+	printf("\n");
+	sortSl(mas, n_row,0,n_col-1);
+	printf("Result:");
+	showMas(mas, n_row, n_col);
+	return 0;
+}
---
+++ TASK2/sort.h
@@ -0,0 +1,24 @@
+#pragma once
+
+int random(int min, int max);
+
+void choiseFunction(int* choise);
+
+void inputChekerElement(int* n, int row, int col);
+
+void inputChekerRow(int* n);
+
+void inputChekerColumn(int* n);
+
+int** memory(int n, int m);
+
+void inputMasByHand(int** mas, int n, int m);
+
+void inputMasRandom(int** mas, int n, int m);
+
+void showMas(int** mas, int n, int m);
+
+void delateRow(int** mas, int* n, int m);
+
+void showResult(int** mas, int n, int m);
+
